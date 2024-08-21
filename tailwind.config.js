e62b449fe02caff60df24267cf2b9005859e8486
zainulabdeen58 <zainsandhu1212@gmail.com>
2024-08-21 20:24:32 +0500

    create and add boiler-plate

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xsm': '320px',
+        // => @media (min-width: 992px) { ... }
+      },
+    },
+  },
+  plugins: [],
+}
+
