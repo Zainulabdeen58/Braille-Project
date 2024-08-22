a6971cbcccfa3e201a7342a5bd53cd3f8c6c1c05
zainulabdeen58 <zainsandhu1212@gmail.com>
2024-08-22 21:42:37 +0500

    create new boiler plate with some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 function App() {
   return (
-    <div className="name xsm:bg-slate-400 xsm:text-5xl text-3xl" >Zain</div>
+    <div className="name xsm:bg-slate-400 xsm:text-5xl text-3xl" >It's working</div>
   );
 }
 
