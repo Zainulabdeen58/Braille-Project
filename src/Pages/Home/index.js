251a0e54cab091564b3ff8524c0e2d619222fbcf
zainulabdeen58 <zainsandhu1212@gmail.com>
2024-09-19 18:08:53 +0500

    do some changing

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Components/Header'
+import Navbar from '../../Components/Navbar'
+function Home() {
+  return (
+    <div>
+      <Header/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
